03eadbdf89a46df9df52d49297e570d59f273fd9
Alan <alansugito75@gmail.com>
2024-05-14 12:05:05 +0700

    add base layer utility

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="font-quicksand text-3xl text-black">Hello World</h1>
-      <h1 className="font-quicksand text-3xl text-primary-blue">Hello World</h1>
-      <h1 className="font-quicksand text-3xl text-success">Hello World</h1>
-      <h1 className="font-quicksand text-3xl text-gold">Hello World</h1>
-      <h1 className="font-quicksand text-3xl text-silver">Hello World</h1>
-      <h1 className="font-quicksand text-3xl text-bronze">Hello World</h1>
-      <h1 className="font-quicksand text-3xl text-danger">Hello World</h1>
+      <h1>Hello World</h1>
     </>
   );
 }
