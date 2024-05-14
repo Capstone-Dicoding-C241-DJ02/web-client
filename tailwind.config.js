0522580d0774e91a107c7e6e60c8257f8f3d5ada
Alan <alansugito75@gmail.com>
2024-05-14 10:07:44 +0700

    tailwind initialization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        quicksand: 'Quicksand',
+      },
+      colors: {
+        black: '#424242',
+        gold: '#FAC337',
+        silver: '#BCBCBC',
+        bronze: '#B77539',
+        danger: '#ED5837',
+        success: '#37ED68',
+        'primary-blue': '#2E3F50',
+      },
+    },
   },
   plugins: [],
-}
-
+};
