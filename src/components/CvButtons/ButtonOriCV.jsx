b37b41efc6c4740d8437bfbbb4c8a5f8da8e543f
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-05-16 11:58:41 +0700

    add 300px width ori&sumbutton

--- src/components/CvButtons/ButtonOriCV.jsx
+++ src/components/CvButtons/ButtonOriCV.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const ButtonOriCV = ({ isActive, onClick }) => {
   return (
     <button
-      className={`px-4 py-2 rounded-md shadow-primary ${
+      className={`px-4 py-2 w-[300px] rounded-md shadow-primary ${
         isActive ? "bg-primary-blue text-white" : "bg-white text-black"
       }`}
       onClick={onClick}
