0653c8c261dd2a7ebc05ecac6afd225225cdadca
Alan <alansugito75@gmail.com>
2024-05-21 20:16:40 +0700

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,56 +9,7 @@ import cvAts from './assets/CV-ATS.pdf';
 
 function App() {
   return (
-    <div className="flex md:gap-7 p-2 w-full h-screen">
-      <SideBar />
-
-      <div className="flex flex-col gap-3 w-full">
-        <CompanyHeader
-          imageUrl={logo}
-          jobTitle="ML Engineer"
-          salary="Rp. 20.000.000"
-          category="Fintech"
-        />
-        <div className="flex flex-col md:flex-row gap-3 h-full">
-          <Leaderboard>
-            <CandidateCard
-              img={logo}
-              number={1}
-              name={'Silo'}
-              position={'ML Engineer'}
-              isActive={true}
-            />
-            <CandidateCard
-              img={logo}
-              number={2}
-              name={'Silo'}
-              position={'ML Engineer'}
-            />
-            <CandidateCard
-              img={logo}
-              number={3}
-              name={'Silo'}
-              position={'ML Engineer'}
-            />
-          </Leaderboard>
-          <div className="space-y-3 w-full">
-            <CandidateHeader
-              img={logo}
-              name="Silo"
-              percentage={90}
-              position="Ml Engineer"
-              yearExperience={2}
-            />
-
-            <CvButtons />
-
-            <div className="rounded bg-white shadow-primary w-full h-full">
-              <iframe src={cvAts} className="w-full h-full"></iframe>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <></>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        quicksand: "Quicksand",
+        quicksand: 'Quicksand',
       },
       colors: {
-        black: "#424242",
-        gold: "#FAC337",
-        silver: "#BCBCBC",
-        bronze: "#B77539",
-        danger: "#ED5837",
-        success: "#37ED68",
-        "primary-blue": "#2E3F50",
+        black: '#424242',
+        gold: '#FAC337',
+        silver: '#BCBCBC',
+        bronze: '#B77539',
+        danger: '#ED5837',
+        success: '#61E041',
+        'primary-blue': '#2E3F50',
       },
       fontSize: {
-        heading1: "2rem",
-        heading2: "1.5rem",
-        body: "1rem",
+        heading1: '2rem',
+        heading2: '1.5rem',
+        body: '1rem',
       },
       boxShadow: {
         primary1:
-          "3px 3px 20px -5px #2e3f5049, 3px 0px 20px -5px #2e3f5049, 0px 3px 20px -5px #2e3f5049",
+          '3px 3px 20px -5px #2e3f5049, 3px 0px 20px -5px #2e3f5049, 0px 3px 20px -5px #2e3f5049',
 
         primary: '2px 3px 20px -5px #2e3f5030',
-
       },
     },
   },
