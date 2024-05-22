4c72ca2c82fca9edb1ce4630c8419c24bf7f9c87
Alan <alansugito75@gmail.com>
2024-05-22 10:11:09 +0700

    reset app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import SideBar from './components/SideBar/SideBar';
-import CompanyHeader from './components/CompanyHeader/CompanyHeader';
-import Leaderboard from './components/Leaderboard';
-import logo from './assets/logo-company.png';
-import CandidateHeader from './components/CandidateHeader';
-import CvButtons from './components/CvButtons/CvButtons';
-import CandidateCard from './components/CandidateCard';
-import cvAts from './assets/CV-ATS.pdf';
-
 function App() {
-  return (
-    <></>
-  );
+  return <></>;
 }
 
 export default App;
