e0247ab8c449dbc0c1e5399a1106e8a6b69f89bc
Alan <alansugito75@gmail.com>
2024-05-29 14:13:49 +0700

    add axios instance

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const BASE_URL = 'BASE_URL'; //e.g http://localhost:8000
+
+const api = (params) =>
+  axios.create({
+    params,
+    baseURL: BASE_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+export default api;
