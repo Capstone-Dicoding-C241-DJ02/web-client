83ff8c6d3362f61d045ac858198832fd52c510d3
alansugito <80502862+AlanSugito@users.noreply.github.com>
2024-05-29 14:18:48 +0700

    add default param to api instance

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'BASE_URL'; //e.g http://localhost:8000
 
-const api = (params) =>
+const api = (params={}) =>
   axios.create({
     params,
     baseURL: BASE_URL,
